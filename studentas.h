0e3a645598f31829c5f5fc27df2b5c990e93f258
Rustizas <rpreikstaitis@gmail.com>
2025-11-27 21:52:15 +0200

    Updatinta Studentas klase kad paveldetu zmogus klase

--- studentas.h
+++ studentas.h
@@ -11,14 +11,13 @@
 #include <sstream>
 #include <chrono>
 #include <algorithm>
+#include "zmogus.h"
 
 using namespace std;
 
-class Studentas
+class Studentas : public Zmogus
 {
 private:
-    string vardas_;
-    string pavarde_;
     vector<int> namudarbai_;
     int egzaminas_;
     double balasVid_;
@@ -26,23 +25,19 @@ private:
 
 public:
     Studentas();
-    Studentas(const string &v, const string &p);
-    Studentas(const string &v, const string &p, const vector<int> &nd, int egz);
-    Studentas(const Studentas &other);
-    Studentas &operator=(const Studentas &other);
+    Studentas(const string& v, const string& p);
+    Studentas(const string& v, const string& p, const vector<int>& nd, int egz);
+    Studentas(const Studentas& other);
+    Studentas& operator=(const Studentas& other);
     ~Studentas();
 
-    string getVardas() const { return vardas_; }
-    string getPavarde() const { return pavarde_; }
     vector<int> getNamudarbai() const { return namudarbai_; }
     int getEgzaminas() const { return egzaminas_; }
     double getBalasVid() const { return balasVid_; }
     double getBalasMed() const { return balasMed_; }
 
-    void setVardas(const string &v) { vardas_ = v; }
-    void setPavarde(const string &p) { pavarde_ = p; }
     void addNamuDarbas(int nd) { namudarbai_.push_back(nd); }
-    void setNamudarbai(const vector<int> &nd) { namudarbai_ = nd; }
+    void setNamudarbai(const vector<int>& nd) { namudarbai_ = nd; }
     void clearNamudarbai() { namudarbai_.clear(); }
     void setEgzaminas(int egz) { egzaminas_ = egz; }
     void setBalasVid(double b) { balasVid_ = b; }
@@ -50,31 +45,32 @@ public:
 
     void skaiciuotiVidurki();
     void skaiciuotiMediana();
+    void isvesti() const override;
 
     bool operator<(const Studentas& other) const { return balasVid_ < other.balasVid_; }
     bool operator>(const Studentas& other) const { return balasVid_ > other.balasVid_; }
     bool operator==(const Studentas& other) const { return vardas_ == other.vardas_ && pavarde_ == other.pavarde_; }
 };
 
-istream& operator>>(istream& is, Studentas& s);
-ostream& operator<<(ostream& os, const Studentas& s);
-
 void bubbleSort(vector<int> &v);
 int randomPazymys(mt19937 &gen);
 void generuotiFaila(const string &failoVardas, int kiekis);
 void generuotiVisusFailus();
 
-void padalintiStudentus_strategija1(const vector<Studentas> &visi,
-                                    double &tRusiavimas, double &tWriteVargs, double &tWriteKiet);
-void padalintiStudentus_strategija1(const list<Studentas> &visi,
-                                    double &tRusiavimas, double &tWriteVargs, double &tWriteKiet);
+void padalintiStudentus_strategija1(const vector<Studentas>& visi,
+                        double& tRusiavimas, double& tWriteVargs, double& tWriteKiet);
+void padalintiStudentus_strategija1(const list<Studentas>& visi,
+                        double& tRusiavimas, double& tWriteVargs, double& tWriteKiet);
 
-void padalintiStudentus_strategija2(vector<Studentas> &visi,
-                                    double &tRusiavimas, double &tWriteVargs, double &tWriteKiet);
-void padalintiStudentus_strategija2(list<Studentas> &visi,
-                                    double &tRusiavimas, double &tWriteVargs, double &tWriteKiet);
+void padalintiStudentus_strategija2(vector<Studentas>& visi,
+                        double& tRusiavimas, double& tWriteVargs, double& tWriteKiet);
+void padalintiStudentus_strategija2(list<Studentas>& visi,
+                        double& tRusiavimas, double& tWriteVargs, double& tWriteKiet);
 
-void padalintiStudentus_strategija3(vector<Studentas> &visi,
-                                    double &tRusiavimas, double &tWriteVargs, double &tWriteKiet);
+void padalintiStudentus_strategija3(vector<Studentas>& visi,
+                        double& tRusiavimas, double& tWriteVargs, double& tWriteKiet);
+
+istream& operator>>(istream& is, Studentas& s);
+ostream& operator<<(ostream& os, const Studentas& s);
 
 #endif
